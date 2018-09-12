2f89d478e5196e667b7ec0709a95bef6e657455f
zhangmingbo <adg7660@hotmail.com>
2018-09-12 23:01:12 +0800

    add 1 file

---
+++ file1.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: file1.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 12 Sep 2018 10:59:27 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello world\n");
+
+    return 0;
+}
